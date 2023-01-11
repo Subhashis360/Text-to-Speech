7963a87a28b499f30a71accd59505acaf918a9e6
Subhashis <113136062+Subhashis360@users.noreply.github.com>
2023-01-12 02:58:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+let text = document.getElementById("textarea");
+const playButton = document.getElementById("submitbtn");
+if ('speechSynthesis' in window) {
+    // console.log("Speech Synthesis is supported 🎉")
+   }else{
+     alert("Text To Speech is not Supported 😞 on your device");
+}
+
+const voicess = window.speechSynthesis.getVoices();
+
+let mainvoice = 1;
+window.onload = function() {
+    let select = document.getElementById("customvoice");
+    const voices = window.speechSynthesis.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+        let option = document.createElement("option");
+            option.value = voices[i].name;
+            option.text = voices[i].name;
+            select.add(option);
+    }
+}
+
+getvala=(texts) => {
+    let selectedanswer = (texts.value)
+    mainvoice = selectedanswer;
+}
+
+document.getElementById("clearbtn").addEventListener('click', ()=>{
+    text.value = "";
+})
+
+playButton.addEventListener("click", abc);
+
+let playing = false;
+
+let volume = 1;
+
+getval=(texts) => {
+    let selectedanswer = (texts.value)
+    volume = selectedanswer;
+}
+
+function abc() {
+    if(text.value === "") {
+        alert("🤘 please Write something I am Very Exited to Speak Your Words 🤘")
+    } else {
+    let msg = new window.SpeechSynthesisUtterance(text.value);
+    if (!playing) {
+        const voices = window.speechSynthesis.getVoices();
+        if(mainvoice === 1) {
+            result = voices[12];
+        } else {
+            result = voices.find(({ name }) => name === mainvoice);
+        }
+        msg.volume = 1; 
+        msg.rate =  volume; 
+        msg.pitch = 1;
+        msg.voice = result;
+        msg.lang = 'en';
+        window.speechSynthesis.speak(msg);
+        playButton.innerHTML = 'Stop';
+        playing = true;
+    } else {
+        speechSynthesis.cancel();
+        playButton.innerHTML = 'play';
+        playing = false;
+    }
+    msg.addEventListener("end", function() {
+        playButton.innerHTML = 'play';
+        playing = false;
+        });
+    }
+}
